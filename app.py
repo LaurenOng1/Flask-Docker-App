041ef64525cfbd2758fb5a4a43f7203b42c9c088
lauren <laurenong96@gmail.com>
2023-12-30 16:19:22 +0800

    Assignment

--- app.py
+++ app.py
@@ -3,8 +3,10 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello_geek():
-    return '<h1>Hello from Flask & Docker</h2>'
+    print('inside here...')
+    return '<h1>Hello from Flask & Docker</h1>'
 
 
 if __name__ == "__main__":
     app.run(debug=True)
+    
